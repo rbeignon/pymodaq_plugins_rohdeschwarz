0b69978692cc6d3464dd2b277cd78677f839a8a7
Aurore Finco <aurore.finco@umontpellier.fr>
2022-11-03 18:46:20 +0100

    added pint in init

--- src/pymodaq_plugins_rohdeschwarz/__init__.py
+++ src/pymodaq_plugins_rohdeschwarz/__init__.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+from pint import UnitRegistry
 
 with open(str(Path(__file__).parent.joinpath('VERSION')), 'r') as fvers:
     __version__ = fvers.read().strip()
+
+ureg = UnitRegistry()
