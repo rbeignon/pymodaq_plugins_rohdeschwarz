7832a2327e7cf23876bd2f683c226651ebdbb92a
Aurore Finco <aurore.finco@umontpellier.fr>
2022-11-04 17:57:28 +0100

    started daq move plugin

--- src/pymodaq_plugins_rohdeschwarz/daq_move_plugins/daq_move_RSMWsources.py
+++ src/pymodaq_plugins_rohdeschwarz/daq_move_plugins/daq_move_RSMWsources.py
@@ -1,62 +1,63 @@
-from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, comon_parameters_fun, main  # common set of parameters for all actuators
-from pymodaq.utils.daq_utils import ThreadCommand # object used to send info back to the main thread
+# common set of parameters for all actuators
+from pymodaq.control_modules.move_utility_classes import DAQ_Move_base, \
+    comon_parameters_fun, main  
+# object used to send info back  to the main thread
+from pymodaq.utils.daq_utils import ThreadCommand
 from pymodaq.utils.parameter import Parameter
-from . import ureg
+# shared UnitRegistry from pint initialized in __init__.py
+from . import ureg, Q_
 from pymodaq_plugins_rohdeschwarz.hardware.SMA_SMB_MW_sources import MWsource
 
-class PythonWrapperOfYourInstrument:
-    #  TODO Replace this fake class with the import of the real python wrapper of your instrument
-    pass
-
-
 class DAQ_Move_RSMWsource(DAQ_Move_base):
-    """Plugin for the Template Instrument
-
-    This object inherits all functionality to communicate with PyMoDAQ Module through inheritance via DAQ_Move_base
-    It then implements the particular communication with the instrument
+    """Plugin for the Rohde & Schwarz microwave sources of SMA and SMB series 
+    This object inherits all functionality to communicate with PyMoDAQ Module
+    through inheritance via DAQ_Move_base
+    It then implements the particular communication with the instrument.
 
     Attributes:
     -----------
-    controller: object
-        The particular object that allow the communication with the hardware, in general a python wrapper around the
-         hardware library
+    controller: MWsource
+        Instance of the class defined to communicate with the device.
     # TODO add your particular attributes here if any
-
     """
-    _controller_units = 'whatever'  # TODO for your plugin: put the correct unit here
-    is_multiaxes = False  # TODO for your plugin set to True if this plugin is controlled for a multiaxis controller
-    axes_names = ['Axis1', 'Axis2']  # TODO for your plugin: complete the list
+    _controller_units = "Hz"
+    is_multiaxes = False  
+    axes_names = [] 
 
     params = [   # TODO for your custom plugin: elements to be added here as dicts in order to control your custom stage
                 ] + comon_parameters_fun(is_multiaxes, axes_names)
 
     def ini_attributes(self):
-        #  TODO declare the type of the wrapper (and assign it to self.controller) you're going to use for easy
-        #  autocompletion
         self.controller: MWsource = None
 
         #TODO declare here attributes you want/need to init with a default value
         pass
 
+    
     def get_actuator_value(self):
-        """Get the current value from the hardware with scaling conversion.
+        """Get the current value of the CW frequency from the hardware.
+        Sends 0 if we are not in CW mode.
 
         Returns
         -------
-        float: The position obtained after scaling conversion.
+        float: The CW frequency in Hz.
         """
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        pos = self.controller.your_method_to_get_the_actuator_value()  # when writing your own plugin replace this line
-        pos = self.get_position_with_scaling(pos)
-        return pos
+        mode, is_running = self._controller.get_status()
+        if mode == "cw":
+            frequency = self.controller.get_frequency()
+        else:
+            self.emit_status("The device is not in CW mode!")
+            frequency = 0 * ureg.Hz
+        freq = self.get_position_with_scaling(frequency.to(ureg.Hz).magnitude)
+        return freq
 
+    
     def close(self):
-        """Terminate the communication protocol"""
-        ## TODO for your custom plugin
-        raise NotImplemented  # when writing your own plugin remove this line
-        #  self.controller.your_method_to_terminate_the_communication()  # when writing your own plugin replace this line
+        """Terminate the communication protocol.
+        """
+        self.controller.close_communication()  
 
+        
     def commit_settings(self, param: Parameter):
         """Apply the consequences of a change of value in the detector settings
 
@@ -70,6 +71,7 @@ class DAQ_Move_RSMWsource(DAQ_Move_base):
            self.controller.your_method_to_apply_this_param_change()
         else:
             pass
+        
 
     def ini_stage(self, controller=None):
         """Actuator communication initialization
